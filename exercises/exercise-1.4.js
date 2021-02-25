814b54bf0e403b55c6bdf778b0bc2399146c72fc
Gia Dat Doan <giadatdoan@gmail.com>
2021-02-25 02:16:49 -0500

    1.4 done

---
+++ exercises/exercise-1.4.js
@@ -0,0 +1,35 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const addUser = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  await client.connect();
+  const db = client.db(req.baseUrl);
+
+  const USER = req.body;
+  await db.collection("users").insertOne({ name: USER.name });
+
+  if (USER) {
+    res.status(201).json({
+      status: 201,
+      data: USER,
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      message: "Error in addUser",
+    });
+  }
+
+  client.close();
+};
+
+module.exports = {
+  addUser,
+};
